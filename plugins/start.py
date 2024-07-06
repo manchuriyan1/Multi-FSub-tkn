617766e97e1af2c8835bef79afc7b929820385b7
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-06 12:31:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,7 +10,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, DELAY
 
 from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user, fsub
+from database.database import add_user, del_user, full_userbase, present_user, fsub, req_db
 from plugins.req_count import add_req, del_req, show_req, join_reqs, reset_req
 
 async def delete_message_after_delay(client: Client, chat_id: int, message_id: int, delay: int):
