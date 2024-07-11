778b5500ccee0cad787dd1ad3ac9d2e432fd7342
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-11 07:20:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,3 @@
-#(©)CodeXBotz
 import os
 import asyncio
 from pyrogram import Client, filters, __version__
@@ -21,7 +20,7 @@ from config import (
     TUT_VID,
     OWNER_ID
 )
-
+from database.token import *
 from database.database import add_user, del_user, full_userbase, present_user, fsub
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
 from shortzy import Shortzy
