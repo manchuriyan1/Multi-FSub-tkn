de54d3a5a5c7efb780b3bdaaa2ba455ea218818a
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-11 07:22:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,7 +20,7 @@ from config import (
     TUT_VID,
     OWNER_ID
 )
-from database.token import *
+from database.token_db import *
 from database.database import add_user, del_user, full_userbase, present_user, fsub
 from helper_func import subscribed, encode, decode, get_messages, get_shortlink, get_verify_status, update_verify_status, get_exp_time
 from shortzy import Shortzy
